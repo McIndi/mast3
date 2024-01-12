c1a0ba953082139cb8f3cb776dbbf2b0410bb2a6
Clifford Bressette <cliffordbressette@mcindi.com>
2024-01-11 20:58:11 -0500

    Fix missing import

--- mast/datapower/web/gui.py
+++ mast/datapower/web/gui.py
@@ -13,7 +13,7 @@
 # along with MAST.  If not, see <https://www.gnu.org/licenses/>.
 #
 # Copyright 2015-2019, McIndi Solutions, All rights reserved.
-from io import StringIO
+from io import StringIO, BytesIO
 from mast.timestamp import Timestamp
 from mast.datapower import datapower
 import pkg_resources
